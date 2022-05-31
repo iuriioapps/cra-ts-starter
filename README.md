51ccd03f8e35d91b1aca18b624607e5783ea2c12
Iurii Okhmat <iuriioapps@gmail.com>
2022-05-31 13:04:23 -0700

    add lightweight gh-pages deployment

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # cra-ts-starter
 Basic CRA with TypeScript
 
-
+## GH deployment configuration
+[spa-github-pages](https://github.com/rafgraph/spa-github-pages)
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ export function App() {
     return (
         <div>
             <Hello message="World" />
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eum explicabo temporibus, in ipsum rem repudiandae ut. Provident deserunt illo quia voluptatum vel, temporibus cupiditate exercitationem eum vitae? Animi nostrum ipsa dolorum quia soluta itaque. Magni quaerat in consequatur temporibus, saepe nesciunt sint debitis animi excepturi minima autem perferendis delectus necessitatibus doloribus, nisi eius recusandae dolorum consequuntur! Ex cumque ad dignissimos soluta odit necessitatibus quo accusantium aspernatur repellendus beatae officia nulla autem facere minima, quis dolorem fugiat deleniti nam omnis corrupti iusto, magnam dolor! Deleniti natus quidem exercitationem perferendis hic aliquid sapiente, blanditiis unde, expedita illo repudiandae, debitis ducimus cumque!</p>
         </div>
     );
 }
